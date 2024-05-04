0d2438f3616313a10d7a49c24247365e359c31f0
webjsmaster <web.js.master@gmail.com>
2024-05-04 21:11:36 +0300

    chore: install and setting tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  textDark: '#403F3D',
+  textLight: '#E1D4C9',
+  textAccent: '#B0907A',
+  bgBody: '#E1D4C9',
+  bgContainer: '#665F55',
+  bgBackdrop: 'rgba(64, 63, 61, 0.3)',
+  borderLight: '#C1B6AD',
+  borderDark: '#665F55',
+};
+
 export default {
   content: [
     "./src/**/*.{js,ts}",
     "./src/**/*"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        current: 'currentColor',
+        ...colors,
+      },
+      boxShadow: {
+        card: '-5px 5px 10px 5px rgba(0, 0, 0, 0.3)',
+      },
+    },
+    screens: {
+      lg: '1280px',
+      md: '768px',
+      tablet: '640px',
+      sm: '440px',
+      min: '120px',
+    },
   },
   plugins: [],
 }
