0fbd821cca36a4069d8fa738d2f2fd0ec92aff37
Frost-704 <boolean.pg.me@gmail.com>
2024-05-05 18:41:59 +0300

    feat: implement api test client and builder

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ctpClient } from '@api/buildClient';
+import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+const projectKey = import.meta.env.VITE_Project_key;
+// Create apiRoot from the imported ClientBuilder and include your Project key
+const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({ projectKey: projectKey });
+
+// Example call to return Project information
+// This code has the same effect as sending a GET request to the commercetools Composable Commerce API without any endpoints.
+const getProject = () => {
+  return apiRoot.get().execute();
+};
+
+// Retrieve Project information and output the result to the log
+getProject().then(console.log).catch(console.error);
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@entity': path.resolve(__dirname, 'src/entity'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@state': path.resolve(__dirname, 'src/state'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      'node-fetch': 'isomorphic-fetch',
+    },
+  },
+  define: {
+    global: {},
+  },
+});
