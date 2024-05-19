0727f4c2783758c3cbd4c84c25b353cda6db8381
Frost-704 <boolean.pg.me@gmail.com>
2024-05-19 22:22:05 +0300

    fix: fixed login/register class

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,30 +1,55 @@
-import Api, { projectKey } from '@api/api';
-import {
-  ByProjectKeyRequestBuilder,
-  ClientResponse,
-  CustomerDraft,
-  CustomerSignin,
-  CustomerSignInResult,
-} from '@commercetools/platform-sdk';
+import { apiInstance, projectKey, session } from '@api/api';
 
-class Auth extends Api {
+import { ByProjectKeyRequestBuilder, CustomerDraft, CustomerSignin } from '@commercetools/platform-sdk';
+import { loaderState, toastState } from '@state/state';
+
+class Auth {
   private readonly customerBuilder: ByProjectKeyRequestBuilder;
 
   constructor() {
-    super();
-    this.customerBuilder = this.apiRoot.withProjectKey({ projectKey });
+    this.customerBuilder = session.withProjectKey({ projectKey });
   }
 
-  async login(user: CustomerSignin): Promise<ClientResponse<CustomerSignInResult>> {
-    return this.customerBuilder.login().post({ body: user }).execute();
+  async login(user: CustomerSignin) {
+    try {
+      const data = await this.customerBuilder.login().post({ body: user }).execute();
+
+      if (data.statusCode === 200) {
+        localStorage.setItem('isAuthorized', 'true');
+        localStorage.setItem('email', user.email);
+        localStorage.setItem('password', user.password);
+        apiInstance.createAuthenticatedSession(user);
+      }
+      return data;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error);
+      }
+    }
   }
 
   async register(user: CustomerDraft) {
     try {
-      return await this.customerBuilder.customers().post({ body: user }).execute();
+      loaderState.getState().loader.show();
+      const data = await this.customerBuilder.customers().post({ body: user }).execute();
+      if (user.password) {
+        if (data.statusCode === 201) {
+          console.log(data.body);
+          console.log('User created');
+          toastState.getState().toast.showSuccess('Registration successful');
+          localStorage.setItem('email', user.email);
+          localStorage.setItem('password', user.password);
+          await this.login({ email: user.email, password: user.password });
+        }
+      }
+      return data;
     } catch (error) {
-      console.error(error);
-      throw error;
+      if (error instanceof Error) {
+        toastState.getState().toast.showError(error.message);
+        console.error(error);
+      }
+    } finally {
+      loaderState.getState().loader.close();
     }
   }
 }
