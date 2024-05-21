bdae33d2259a025118e896fa69e329985048cc34
Frost-704 <boolean.pg.me@gmail.com>
2024-05-22 01:16:25 +0300

    fix: fix registration and age validate

--- src/utils/validation/date.ts
+++ src/utils/validation/date.ts
@@ -1,11 +1,13 @@
 export function validationDate(value: string): string[] {
   const resultMessages: string[] = [];
-
   const userDate = new Date(value);
   const currentDate = new Date();
   const minAge = 13;
   const userAge = currentDate.getFullYear() - userDate.getFullYear();
-  const isValidAge = userAge >= minAge;
+  const isBirthdayPassed =
+    currentDate.getMonth() > userDate.getMonth() ||
+    (currentDate.getMonth() === userDate.getMonth() && currentDate.getDate() >= userDate.getDate());
+  const isValidAge = userAge > minAge || (userAge === minAge && isBirthdayPassed);
 
   const textError = 'You must be 13 years old or older.';
 
