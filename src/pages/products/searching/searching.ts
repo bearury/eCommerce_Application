73a7042e73e49ec170934ccc32d4bc9b46065a92
anastshak <nastyashakura@gmail.com>
2024-06-03 22:13:10 +0300

    feat: add searching

---
+++ src/pages/products/searching/searching.ts
@@ -0,0 +1,67 @@
+import View from '@utils/view.ts';
+import { ElementCreator, ParamsElementCreator } from '@utils/element-creator.ts';
+import styles from './searching.module.scss';
+import Search from '@api/search';
+import { apiInstance } from '@api/api';
+import { productsDataState, toastState } from '@state/state';
+
+export default class SearchingField extends View {
+  input: HTMLElement;
+
+  searchBtn: HTMLElement;
+
+  constructor() {
+    const params: ParamsElementCreator = {
+      tag: 'div',
+      classNames: [styles.searchField],
+      textContent: 'Search:',
+    };
+    super(params);
+
+    this.input = new ElementCreator({
+      tag: 'input',
+      classNames: [styles.inputField],
+      attribute: [{ type: 'placeholder', value: 'Please, enter your request' }],
+    }).getElement();
+
+    this.searchBtn = new ElementCreator({
+      tag: 'button',
+      textContent: 'search',
+      classNames: [styles.searchBtn],
+      callback: [{ event: 'click', callback: this.searchProduct.bind(this) }],
+    }).getElement();
+
+    this.configureView();
+  }
+
+  private searchProduct() {
+    const inputValue = this.input as HTMLInputElement;
+    const search = new Search(apiInstance);
+    console.log(inputValue.value);
+
+    search
+      .search({
+        'text.en-US': inputValue.value,
+        fuzzy: true,
+        fuzzyLevel: 1,
+      })
+      .then((data) => {
+        productsDataState.getState().setData(data);
+      })
+      .catch(() => {
+        toastState
+          .getState()
+          .toast.showError('Invalid Request! Please, enter a clearer query (and more than 2 real characters).');
+      });
+  }
+
+  private configureView(): void {
+    const searchField: HTMLElement = this.getElement();
+    document.addEventListener('keydown', (event) => {
+      if (event.code == 'Enter') {
+        this.searchProduct();
+      }
+    });
+    searchField.append(this.input, this.searchBtn);
+  }
+}
