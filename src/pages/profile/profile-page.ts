1bae63504c092c1f80670dd88492ae4f1c7bc23c
webjsmaster <web.js.master@gmail.com>
2024-06-05 17:49:53 +0300

    fix/render_select_categories

--- src/pages/profile/profile-page.ts
+++ src/pages/profile/profile-page.ts
@@ -1,6 +1,5 @@
 import View from '@utils/view.ts';
 import { ElementCreator, ParamsElementCreator } from '@utils/element-creator.ts';
-import inputStyles from '@components/input/input-field/input-field.module.scss';
 import styles from './profile-page.module.scss';
 import Container from '@components/container/container';
 import CustomerApi from '@api/customerApi';
@@ -16,7 +15,7 @@ export default class ProfilePage extends View {
   customerId: string = localStorage.getItem('customerID') || '';
 
   customerApi: CustomerApi = new CustomerApi(apiInstance);
-  
+
   shippingAddresses: ElementCreator;
 
   billingAddresses: ElementCreator;
@@ -26,7 +25,7 @@ export default class ProfilePage extends View {
   shippingTitle: ElementCreator;
 
   billingTitle: ElementCreator;
-  
+
   constructor() {
     const params: ParamsElementCreator = {
       tag: 'section',
@@ -154,6 +153,7 @@ export default class ProfilePage extends View {
     if (addressType === 'shipping') this.shippingAddresses.getElement().appendChild(address);
     if (addressType === 'billing') this.billingAddresses.getElement().appendChild(address);
   }
+
   private async setCustomerAddresses(
     addressesInfo: Pick<
       Customer,
@@ -202,6 +202,7 @@ export default class ProfilePage extends View {
       });
     }
   }
+
   private getAllAddresses() {
     console.log(this.shippingAddresses.element.childNodes);
   }
