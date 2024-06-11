4d77f8c81d9859761db75fe2bbf65f97aa86e942
anastshak <nastyashakura@gmail.com>
2024-06-12 01:05:50 +0300

    feat: add info about empty cart

--- src/pages/basket/basket-page.ts
+++ src/pages/basket/basket-page.ts
@@ -1,9 +1,13 @@
 import View from '@utils/view.ts';
-import { ParamsElementCreator } from '@utils/element-creator.ts';
+import { ElementCreator, ParamsElementCreator } from '@utils/element-creator.ts';
 import styles from './basket-page.module.scss';
 import Router from '@router/router.ts';
+import Image from '@components/image/image';
+import emptyCart from '/empty-cart.png';
 
 export default class BasketPage extends View {
+  emptyCartBlock: HTMLElement;
+
   router: Router;
 
   constructor(router: Router) {
@@ -14,12 +18,29 @@ export default class BasketPage extends View {
     super(params);
     this.router = router;
 
+    this.emptyCartBlock = new ElementCreator({
+      tag: 'div',
+      classNames: [styles.emptyCartBlock],
+    }).getElement();
+
     this.configureView();
   }
 
   private configureView(): void {
     const about = this.getElement();
+    const cartImage = new Image({ classNames: [styles.image], img: emptyCart }).getElement();
+    const emptyCartMsg = new ElementCreator({
+      tag: 'div',
+      classNames: [styles.emptyCartMsg],
+      textContent: 'Your cart is empty!',
+    }).getElement();
+    const emptyCartMsgLink = new ElementCreator({
+      tag: 'div',
+      classNames: [styles.emptyCartMsgLink],
+      textContent: 'Looks like you have not added anything to your cart...',
+    }).getElement();
 
-    about.textContent = 'This is basket page';
+    this.emptyCartBlock.append(cartImage, emptyCartMsg, emptyCartMsgLink);
+    about.append(this.emptyCartBlock);
   }
 }
